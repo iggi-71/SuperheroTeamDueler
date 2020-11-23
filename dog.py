1dbcd96d71a39c55d74373244d26efced0724c9e
iggi-71 <jimenez72iggi@gmail.com>
2020-11-22 19:41:31 -0800

    done with the first part

---
+++ dog.py
@@ -0,0 +1,15 @@
+# dog.py
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bark(self):
+        print("Woof!")
+
+    def roll(self):
+        print("<> rolls over")
+
+    def sit(seld):
+        print("<> sits")
---
+++ my_dog.py
@@ -0,0 +1,19 @@
+# my_dogs.py
+import dog # we need to specify exactly what we want
+
+my_dog = dog.Dog("Rex", "SuperDog")
+my_dog.bark()
+
+my_other_dog = dog.Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+first_dog = dog.Dog("Garfield", "Beagle")
+second_dog = dog.Dog("Snoop Dog", "Bulldog")
+third_dog = dog.Dog("Dogzilla", "Poodle")
+print(first_dog.name, second_dog.breed)
+print(second_dog.name, second_dog.breed)
+print(third_dog.name, second_dog.breed)
+
+print(first_dog.bark())
+print(second_dog.roll())
+print(third_dog.sit())
