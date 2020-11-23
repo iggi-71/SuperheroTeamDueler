f960e4d0c5e447f6a45424240055bf88ffe87f10
iggi-71 <jimenez72iggi@gmail.com>
2020-11-23 14:34:08 -0800

    first part of the 2nd section works

---
+++ ability.py
@@ -0,0 +1,16 @@
+import random
+
+    #  Created an ability class with an attack method'''
+class Ability:
+    def __init__(self, name, max_damage):
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        random_value = random.randint(0,self.max_damage)
+        return random_value
+
+if __name__ == "__main__":
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack()) 
